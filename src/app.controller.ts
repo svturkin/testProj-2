fb7675806501f7af9795b4b9779c22cc91df5afb
svturkin <70198600+svturkin@users.noreply.github.com>
2021-11-19 18:01:18 +0300

    fixes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,7 +9,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('nodejs/:branchName')
-  getNodejsCommitsInfo(@Param('branchName') branchName: string): Promise<ResponseDto> {
+  getNodejsCommitsInfo(@Param('branchName') branchName: string): Promise<ResponseDto[]> {
     return this.appService.getNodejsCommitsInfo(branchName)
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,7 @@ import { ResponseDto } from "./dto/response.dto";
 
 @Injectable()
 export class AppService {
-  async getNodejsCommitsInfo(branchName: string): Promise<ResponseDto> {
+  async getNodejsCommitsInfo(branchName: string): Promise<ResponseDto[]> {
       try {
         const response = await got(`https://api.github.com/repos/nodejs/node/commits?per_page=25&sha=${branchName}`);
         return JSON.parse(response.body).map(el => {
